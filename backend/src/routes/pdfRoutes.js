06c2066c2f60e093108fa68d727b3a08402c8fd2
nikolasgian10 <nikolasgian10@gmail.com>
2025-06-12 17:57:01 -0300

    fix: Corrige o caminho de destino do Multer em pdfRoutes.js

--- backend/src/routes/pdfRoutes.js
+++ backend/src/routes/pdfRoutes.js
@@ -7,7 +7,7 @@ const pdfController = require('../controllers/pdfController');
 // Configuração do Multer para upload de arquivos
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, path.join(__dirname, '../../../uploads'));
+        cb(null, path.join(__dirname, '..', '..', 'uploads'));
     },
     filename: function(req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
